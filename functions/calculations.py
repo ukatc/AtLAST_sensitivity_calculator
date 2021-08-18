fd2eecf5510918d0e4560bed150d74db1cca4ee4
jtr6 <joanna.ramasawmy@stfc.ac.uk>
2021-08-18 17:33:20 +0100

    adding structure to calculations

---
+++ functions/calculations.py
@@ -0,0 +1,6 @@
+# Will need to get to the parameters required for the sensitivity calculation: bandwidth, tau_atm, SEFD, n_pol, eta_s, and either t_int or S.
+# Bandwidth comes from config
+# tau_atm comes from atm_model
+
+
+def SEFD()
