349f1f0c6e2079317e893921fb99afbac76a4804
jtr6 <joanna.ramasawmy@stfc.ac.uk>
2021-08-18 14:56:51 +0100

    first commit

---
+++ testing_calculations.py
@@ -0,0 +1,13 @@
+# 2021/06/01
+#
+# First attempt at making code do the sensitivity calculations - rough version!
+
+import numpy as np
+
+# Let's define some variables
+
+eta_feed = 1
+
+eta_dish = 1
+
+eta_A = eta_dish * eta_feed
