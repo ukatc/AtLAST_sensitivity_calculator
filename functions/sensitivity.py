0dd66a7d44b9e928d78647a90305a0e4ac772b33
jtr6 <joanna.ramasawmy@stfc.ac.uk>
2021-08-19 13:27:35 +0100

    re-structured into class

--- functions/sensitivity.py
+++ functions/sensitivity.py
@@ -1,16 +1,26 @@
 import astropy.units as u
 import numpy as np
 
-def calc_sensitivity(bandwidth, tau_atm, SEFD, n_pol, eta_s, t_int):
-    '''
-    Return sensitivity of telescope (Jansky) for a given integration time t_int
-    '''
-    S = SEFD/(eta_s * np.sqrt(n_pol * bandwidth * t_int))
-    return S * np.exp(tau_atm)
+class Calculator:
+    def __init__(self, bandwidth, tau_atm, sefd, n_pol, eta_s, sensitivity=0, t_int=0):
+        self.bandwidth = bandwidth
+        self.tau_atm = tau_atm
+        self.sefd = sefd
+        self.n_pol = n_pol
+        self.eta_s = eta_s
+        self.sensitivity = sensitivity
+        self.t_int = t_int
+        
+    def calc_sensitivity(self):
+        '''
+        Return sensitivity of telescope (Jansky) for a given integration time t_int
+        '''
+        sensitivity = self.sefd/(self.eta_s * np.sqrt(self.n_pol * self.bandwidth * self.t_int))
+        return sensitivity * np.exp(self.tau_atm)
 
-def calc_t_integration(bandwidth, tau_atm, SEFD, n_pol, eta_s, S):
-    '''
-    Return integration time required for some sensitivity S (Jansky) to be reached.
-    '''
-    t_int = (np.exp(tau_atm) * SEFD / S * eta_s)**2 / (n_pol * bandwidth)
-    return t_int
+    def calc_t_integration(self):
+        '''
+        Return integration time required for some sensitivity S (Jansky) to be reached.
+        '''
+        t_int = (np.exp(self.tau_atm) * self.sefd / self.sensitivity * self.eta_s)**2 / (self.n_pol * self.bandwidth)
+        return t_int
