d55e7f935041ab560d7410ebf8e6551615430e19
jtr6 <joanna.ramasawmy@stfc.ac.uk>
2021-10-18 16:22:14 +0100

    made SEFD its own class file

---
+++ src/functions/sefd.py
@@ -0,0 +1,19 @@
+# Just the SEFD calculation in here. Will need input from system_temperature and efficiencies (some other calculation?)
+
+from astropy import constants
+
+class SEFD:
+    def __init__(self) -> None:
+        pass
+
+    def calculate(T_sys, area, eta_A):
+        '''
+        Calculate SEFD from the system temperature T_sys, the dish area and the efficiency eta_A
+        :param T_sys: system temperature
+        :type T_sys: astropy.units.Quantity
+        :param area: the dish area 
+        :type area: astropy.units.Quantity
+        :param eta_A: the efficiency factor
+        :type eta_A: float
+        '''
+        return (2 * constants.k_B * T_sys)/(eta_A * area)
