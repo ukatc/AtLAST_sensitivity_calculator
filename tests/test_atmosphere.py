00cd7d5fcff37f65a01f97ace50587aca6234acc
jtr6 <joanna.ramasawmy@stfc.ac.uk>
2021-12-21 12:02:49 +0000

    testing fixed

--- tests/test_atmosphere.py
+++ tests/test_atmosphere.py
@@ -3,37 +3,31 @@ import numpy as np
 from pathlib import Path
 import pytest
 
-STATIC_DATA_PATH = Path(__file__).resolve().parents[1] / "static"
-
-WEATHER_PWV = np.array([5, 10, 20])
-T_ATM_PATH = STATIC_DATA_PATH / "lookups" / "T_atm_SKA_test.txt"
-TAU_ATM_PATH = STATIC_DATA_PATH / "lookups" / "tau_atm_SKA_test.txt"
-
 def test_init():
     from src.functions.atmosphere_params import AtmosphereParams
-    obs_freq = 40 * u.GHz
-    pwv = 0.5
+    obs_freq = 400 * u.GHz
+    pwv = 50
     elevation = 20 * u.deg
     atm = AtmosphereParams(obs_freq, pwv, elevation)
-    assert atm.tau_atm_table[0,0] == 2.50
-    assert atm.T_atm_table[0,0] == 2.50
+    assert atm.tau_atm_table[0,0] == 30
+    assert atm.T_atm_table[0,0] == 30
 
 def test_tau_atm():
     from src.functions.atmosphere_params import AtmosphereParams
-    obs_freq = 41 * u.GHz
-    pwv = 0.5
+    obs_freq = 500 * u.GHz
+    pwv = 50
     elevation = 20 * u.deg
     atm = AtmosphereParams(obs_freq, pwv, elevation)
     zenith = 90.0 * u.deg - elevation
 
-    assert (atm.tau_atm().value < 6e-3/np.cos(zenith)) and (atm.tau_atm().value > 5e-3/np.cos(zenith))
+    assert (atm.tau_atm().value < 1.0419/np.cos(zenith)) and (atm.tau_atm().value > 1.040/np.cos(zenith))
 
 
 def test_T_atm():
     from src.functions.atmosphere_params import AtmosphereParams
-    obs_freq = 41 * u.GHz
-    pwv = 0.5
+    obs_freq = 500 * u.GHz
+    pwv = 50
     elevation = 20 * u.deg
     atm = AtmosphereParams(obs_freq, pwv, elevation)
 
-    assert (atm.T_atm().value > 1.40) and (atm.T_atm().value < 1.43)
+    assert (atm.T_atm().value > 173.668) and (atm.T_atm().value < 173.766)
