748185609a505d407924f88fd3fd4c12310cda22
jtr6 <joanna.ramasawmy@stfc.ac.uk>
2022-03-24 13:31:31 +0000

    added test placeholders for efficiencies

--- tests/test_efficiencies.py
+++ tests/test_efficiencies.py
@@ -22,3 +22,14 @@ def test_eta_a():
     print(eta_a)
     # assert eta_a.value == pytest.approx(0.9211434072170483)
     assert eta_a.value <= 1
+
+def test_ones():
+    '''
+    can write a test here that if all efficiencies are set to 1, the resulting efficiency should be 1
+    '''
+
+def test_zero():
+    '''
+    can write a test here that if any efficience is 0, the resulting efficiency should be zero
+    '''
+    
