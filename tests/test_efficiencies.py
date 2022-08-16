6529f9facc5a90ad1a1a7d49b63adb1d380d975a
jtr6 <joanna.ramasawmy@stfc.ac.uk>
2022-08-16 16:06:53 +0100

    updated testing

--- tests/test_efficiencies.py
+++ tests/test_efficiencies.py
@@ -4,12 +4,17 @@ import astropy.units as u
 def test_eta_ill():
     from atlast_sc.efficiencies import Efficiencies
     test_ill = 0.5
-    eff = Efficiencies(test_ill)
+    eta_q = 1
+    eta_spill = 1
+    eta_block = 1
+    eta_pol = 1
+    eta_r = 1
+    eff = Efficiencies(test_ill, eta_q, eta_spill, eta_block, eta_pol, eta_r)
     assert eff.eta_ill == test_ill
 
 def test_eta_s():
     from atlast_sc.efficiencies import Efficiencies
-    eff = Efficiencies(0.7)
+    eff = Efficiencies(1,1,1,1,1,1)
     eta_s = eff.eta_s()
     assert eta_s <= 1
 
@@ -17,7 +22,7 @@ def test_eta_a():
     from atlast_sc.efficiencies import Efficiencies
     obs_freq = 500 * u.GHz
     surface_rms = 25 * u.micron
-    eff = Efficiencies(0.7)
+    eff = Efficiencies(0.7,1,1,1,1,1)
     eta_a = eff.eta_a(obs_freq, surface_rms)
     print(eta_a)
     # assert eta_a.value == pytest.approx(0.9211434072170483)
@@ -32,4 +37,6 @@ def test_zero():
     '''
     can write a test here that if any efficience is 0, the resulting efficiency should be zero
     '''
+    
+
     
