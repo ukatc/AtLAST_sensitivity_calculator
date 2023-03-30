b19c4ca1bc53664ac6f29d983e38f38816c02f5c
fkirton <fiona.kirton@gmail.com>
2023-03-30 17:29:39 +0100

    Display calculated values; set/unset 'recalculate' state of UI

--- fastapi_web_client/scripts/calculator.js
+++ fastapi_web_client/scripts/calculator.js
@@ -3,8 +3,17 @@ import {validateInput} from './validators.js'
 
 $(document).ready(() => {
 
-    const highlightRecalculate = () => {
-        // TODO: do I really want to do this?
+    let calculationPerformed = false;
+
+    const setRecalculateState = () => {
+        // Change the text and style of the 'Calculate' button
+        const calculateBtn = document.getElementById("calculate");
+        calculateBtn.innerHTML = "Recalculate";
+        calculateBtn.classList.remove("btn-primary");
+        calculateBtn.classList.add("btn-danger");
+
+        // Change the style of the output card
+        document.querySelector("#output").classList.add("recalculate");
     }
 
     const doCalculation = (param_data) => {
@@ -22,14 +31,15 @@ $(document).ready(() => {
         }
 
         // Find which of the two calculation options are checked
-        const calc_options =
+        const calcOptions =
             document.querySelectorAll('input[name="calc-options"');
 
-        for (const option of calc_options) {
+        for (const option of calcOptions) {
             if (option.checked) {
                 calculate(inputData, option.getAttribute("calculation"))
                 .then((data) => {
-                    console.log('got response', data);
+                    showCalculatedValue(data.value, data.unit);
+                    calculationPerformed = true;
                 })
                 .catch((error) => {
                     // TODO handle the error
@@ -41,6 +51,19 @@ $(document).ready(() => {
         }
     }
 
+    const showCalculatedValue = (value, unit) => {
+
+        // Convert the value to a float and round to 4 decimal places
+        const roundedVal = (+value).toFixed(4);
+
+        const outputBox = document.querySelector("#output");
+
+        // Make sure the output isn't in the 'recalculate' state
+        document.querySelector("#output").classList.remove("recalculate");
+
+        output.innerHTML = `${roundedVal} ${unit}`;
+    }
+
     // Hide all of the invalid-message divs
     const allInvalidMessages =
         document.querySelectorAll(".invalid-message");
@@ -48,21 +71,56 @@ $(document).ready(() => {
         input.hidden = true;
     });
 
+    // Initialize the calculation radio buttons (select integration time as
+    //   the default)
+    const calcOptions = document.querySelectorAll('input[name="calc-options"');
+    for (const option of calcOptions) {
+        // Check integration time by default
+        if (option.id === "btn-t-int") {
+            option.checked = true;
+        }
+
+        // Add an event listener to toggle the 'disabled' attribute of the
+        //   radio button options
+        option.addEventListener("click", (e) => {
+            // Iterate over the radio buttons. For the one that's clicked,
+            // hide the corresponding input box, otherwise show the
+            // corresponding box.
+            for (const opt of calcOptions) {
+                // Strip 'btn' from the id prepend with 'row-2'.
+                // This can be used to match the name of the corresponding
+                // input box row.
+                const inputBoxRowId =
+                    "row-" + opt.id.replace("btn-", "");
+                const inputBoxRow = document.getElementById(inputBoxRowId);
+
+                if (opt === e.target) {
+                    inputBoxRow.classList.add("d-none");
+                } else {
+                    inputBoxRow.classList.remove("d-none");
+                }
+
+                // Set the 'recalculate' state of the UI
+                setRecalculateState();
+            }
+        })
+    }
+
     geParamValuesUnits()
         .then((data) => {
             let formValidated = false;
 
-            // Set up the user input field on all input fields
+            // Set up the user input field for all parameters
             const allUserInput = document.querySelectorAll(".param-input");
 
             allUserInput.forEach(input => {
                 // Add the placeholder text
                 input.setAttribute("placeholder", "Enter a value...");
 
-                // Set the required attribute
+                // Make the input required
                 input.required = true;
 
-                // Validate the initial input
+                // Validate the initial input data (should never fail!)
                 formValidated = validateInput(input, data[input.name]);
 
                 // Add an event listener to validate input when it changes
@@ -73,6 +131,11 @@ $(document).ready(() => {
                 });
             });
 
+            // Calculate the integration time using the default values
+            doCalculation(data);
+
+            // Add an event listener to do the calculation when the form is
+            //  submitted
             const form = document.getElementById("calculator-form");
             form.addEventListener("submit", (e) => {
                 e.preventDefault();
