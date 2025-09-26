3329f742082ce56e00453c99647f945fcfe3a876
ilgin.okan <ilgin.okan@stfc.ac.uk>
2025-09-26 15:34:56 +0100

    ASC-76 Removed unused imports and improved readability

--- atlast_sc/factory.py
+++ atlast_sc/factory.py
@@ -1,17 +1,12 @@
 from atlast_sc.calculator import Calculator
 from atlast_sc.parameter_setup import ParameterSetup
-from atlast_sc.parameters.user_input_parameters import UserInputParameters
-from atlast_sc.parameters.instrument_specific_parameters import InstrumentSpecificParameters
-from atlast_sc.parameters.telescope_and_environment_parameters import TelescopeAndEnvironmentParameters
-from atlast_sc.parameters.derived_parameters import DerivedParameters
-
 
 class CalculatorFactory:
-    def __init__(self, user_input={}, instrument_name="default"):
+    def __init__(self, user_input={}):
         if user_input:
-            self.calculator = self._create_calculator(ParameterSetup(user_input=user_input))
+            self.calculator = self._create_calculator(param_setup=ParameterSetup(user_input=user_input))
         else: # use the default values
-            self.calculator = self._create_calculator(ParameterSetup())
+            self.calculator = self._create_calculator(param_setup=ParameterSetup())
 
     #####################
     # Protected methods #
