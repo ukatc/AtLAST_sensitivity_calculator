114ae8af7e161e6f24293649a98941c5bea91507
ilgin.okan <ilgin.okan@stfc.ac.uk>
2025-09-26 12:03:59 +0100

    Added/modified comments for clarity

--- atlast_sc/parameters/user_input_parameters.py
+++ atlast_sc/parameters/user_input_parameters.py
@@ -16,34 +16,24 @@ class UserInputParameters:
 
     def __init__(self, param_setup):
         self._param_setup = param_setup
-        # Note: We are calculating and storing the derived parameters in this class
+        # NOTE: We are calculating and storing the derived parameters in this class
         # as they will be recalculated according to new user input.
         self._derived_parameters_model = self._calculate_derived_parameters() 
         self._derived_parameters = DerivedParameters(self._derived_parameters_model)
 
-    # TODO t_int and sensitivity are a special can se. They can be both
-    #   set and calculated. Special care needs to be taken on setting them:
-    #   they will have to be validated if they're set, but not calculated.
-    #   Also, if they're set, the user needs to be warned if they then try
-    #   to use Calculator values with redoing the senstivity/integration time
-    #   calculation
     @property
     def t_int(self):
         """
-        Get or set the integration time
+        Get the integration time that user has specified
         """
         return self._param_setup.calculation_inputs.user_input.t_int.value
 
     @t_int.setter
     @Decorators.validate_value
     def t_int(self, value):
-        # TODO: Setting this value in the on the inputs feels wrong.
-        #  This may be a calculated param. Allowing the user to change it
-        #  without restriction may lead to inaccurate output if they perform
-        #  the sensitivity calculation, change the integration time, then
-        #  store or use those stored values.
-        #  Need separate "outputs" properties that are used for
-        #  subsequent calculations and/or storing results?
+        """
+        Set the integration time that user has specified
+        """
         # TODO: We don't technically need to update the unit here (ditto other
         #   values with units, because the value is set to a Quantity, which
         #   contains the units. It's this value that is used throughout the
@@ -56,95 +46,117 @@ class UserInputParameters:
     @property
     def sensitivity(self):
         """
-        Get or set the sensitivity
+        Get the sensitivity that user has specified
         """
         return self._param_setup.calculation_inputs.user_input.sensitivity.value
 
     @sensitivity.setter
     @Decorators.validate_value
     def sensitivity(self, value):
-        # TODO: Setting this value in the on the inputs feels wrong.
-        #  This may be a calculated param. Allowing the user to change it
-        #  without restriction may lead to inaccurate output if they perform
-        #  the sensitivity calculation, change the integration time, then
-        #  store or use those stored values.
-        #  Need separate "outputs" properties that are used for
-        #  subsequent calculations and/or storing results?
+        """
+        Set the sensitivity that user has specified
+        """
         self._param_setup.calculation_inputs.user_input.sensitivity.value = value
         self._param_setup.calculation_inputs.user_input.sensitivity.unit = value.unit
 
     @property
     def bandwidth(self):
         """
-        Get or set the bandwidth
+        Get the bandwidth
         """
         return self._param_setup.calculation_inputs.user_input.bandwidth.value
-    # With the new calculation of SEFD over the whole frequency range, SEFD is now dependent on bandwidth and so parameters must be updated every time bandwidth is changed.
+    
+    # With the new calculation of SEFD over the whole frequency range, SEFD is now dependent
+    # on bandwidth and so parameters must be updated every time bandwidth is changed.
     @bandwidth.setter
     @Decorators.validate_and_update_params
     def bandwidth(self, value):
+        """
+        Set the bandwidth
+        """
         self._param_setup.calculation_inputs.user_input.bandwidth.value = value
         self._param_setup.calculation_inputs.user_input.bandwidth.unit = value.unit
 
     @property
     def obs_freq(self):
         """
-        Get or set the sky frequency of the observations
+        Get the sky frequency of the observations
         """
         return self._param_setup.calculation_inputs.user_input.obs_freq.value
 
     @obs_freq.setter
     @Decorators.validate_and_update_params
     def obs_freq(self, value):
+        """
+        Set the sky frequency of the observations
+        """
         self._param_setup.calculation_inputs.user_input.obs_freq.value = value
         self._param_setup.calculation_inputs.user_input.obs_freq.unit = value.unit
 
     @property
     def n_pol(self):
         """
-        Get or set the number of polarisations being observed
+        Get the number of polarisations being observed
         """
         return self._param_setup.calculation_inputs.user_input.n_pol.value
 
     @n_pol.setter
     @Decorators.validate_value
     def n_pol(self, value):
+        """
+        Set the number of polarisations being observed
+        """
         self._param_setup.calculation_inputs.user_input.n_pol.value = value
 
     @property
     def weather(self):
         """
-        Get or set the relative humidity
+        Get the relative humidity
         """
         return self._param_setup.calculation_inputs.user_input.weather.value
 
     @weather.setter
     @Decorators.validate_and_update_params
     def weather(self, value):
+        """
+        Set the relative humidity
+        """
         self._param_setup.calculation_inputs.user_input.weather.value = value
 
     @property
     def elevation(self):
         """
-        Get or set the elevation of the target for calculating air mass
+        Get the elevation of the target for calculating air mass
         """
         return self._param_setup.calculation_inputs.user_input.elevation.value
 
     @elevation.setter
     @Decorators.validate_and_update_params
     def elevation(self, value):
+        """
+        Set the elevation of the target for calculating air mass
+        """
         self._param_setup.calculation_inputs.user_input.elevation.value = value
         self._param_setup.calculation_inputs.user_input.elevation.unit = value.unit
 
     @property
     def derived_parameters(self):
         """
-        Parameters calculated from user input and instrument specific
+        Get derived parameters calculated from user input and instrument specific parameters
         """
+        # NOTE: Derived parameters object live in user input class because it is 
+        # recalculated every time a user input is changed. With the refactored version 
+        # of the software, it made more sense to keep the calculation of the derived 
+        # parameters and the current state of the derived parameters within UIP. 
+        # This is not the “best” solution per se, however, given the time constraints 
+        # it's the best solution -that relatively makes sense- for now.
         return self._derived_parameters
 
     @derived_parameters.setter
     def derived_parameters(self, value):
+        """
+        Set derived parameters calculated from user input and instrument specific parameters
+        """
         self._derived_parameters = value
 
     def _calculate_derived_parameters(self):
