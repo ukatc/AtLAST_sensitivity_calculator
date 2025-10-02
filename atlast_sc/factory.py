095a59cac1e64bb77245a92dd03977af42c67a54
ilgin.okan <ilgin.okan@stfc.ac.uk>
2025-10-02 09:54:43 +0100

    ASC-7 Removed unused imports from factory class

--- atlast_sc/factory.py
+++ atlast_sc/factory.py
@@ -1,10 +1,5 @@
 from atlast_sc.calculator import Calculator
 from atlast_sc.parameter_setup import ParameterSetup
-from atlast_sc.parameters.user_input_parameters import UserInputParameters
-from atlast_sc.parameters.instrument_specific_parameters import InstrumentSpecificParameters
-from atlast_sc.parameters.telescope_and_environment_parameters import TelescopeAndEnvironmentParameters
-from atlast_sc.parameters.derived_parameters import DerivedParameters
-
 
 class CalculatorFactory:
     def __init__(self, user_input={}, instrument_name="default"):
