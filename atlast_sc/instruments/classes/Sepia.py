33a7e0cb17e0912317f3619715db6dff5237af24
ilgin.okan <ilgin.okan@stfc.ac.uk>
2025-12-04 17:33:17 +0000

    ASC-70 Added Sepia specific receiver temperature calculation method to Sepia's python module

--- atlast_sc/instruments/classes/Sepia.py
+++ atlast_sc/instruments/classes/Sepia.py
@@ -1,3 +1,4 @@
+import re
 import astropy.units as u
 from atlast_sc.instrument import Instrument
 
@@ -21,21 +22,52 @@ class Sepia(Instrument):
     def T_rx(self, value):
         self._T_rx = value
 
+    @property 
+    def T_sys(self):
+        return self._T_sys
+    
+    @T_sys.setter
+    def T_sys(self, value):
+        self._T_sys = value
+
     ################################################
     # Additional instrument specific methods below #
     ################################################
 
     def calculate_receiver_temp(self, obs_freq):
-        # NOTE: This will be populated with instrument specific 
-        # receiver temperature calculation equation.
-        temp_option = float(self.receiver_temp_options_and_unit['values'][0])
-        temp = temp_option * u.K
+        temp_options = re.findall(r"[\d.]+", 
+                           self.receiver_temp_options_and_unit['values'][0])
+        temp_options = [float(temp) for temp in temp_options]
+        freq_options = self.obs_freq_ranges_and_unit['ranges']
+
+        freq_ranges = []
+
+        for freq_range in freq_options:
+            range = re.findall(r"[\d.]+", freq_range)
+            range = [float(val) for val in range]
+            freq_ranges.append(range)
+
+        obs_freq = obs_freq.value
+        t_rx_low = temp_options[0]
+        t_rx_high = temp_options[1]
+        freq_1 = 370 * u.GHz
+
+        if obs_freq >= freq_ranges[0][0] and obs_freq <= freq_ranges[0][1]:
+            temp = t_rx_low * u.K
+        elif obs_freq > freq_ranges[1][0] and obs_freq <= freq_ranges[1][1]:
+            temp = t_rx_low + (t_rx_high - t_rx_low) * (obs_freq-freq_ranges[1][0]) / \
+                (freq_1 - obs_freq-freq_ranges[1][0]) * u.K
+            
         self.T_rx = temp
         return temp
     
     @staticmethod
     def _set_default_receiver_temp(receiver_temp_options_and_unit):
-        receiver_temp = u.Quantity(receiver_temp_options_and_unit['values'][0],
-                                    u.K)
-        return receiver_temp
+        temp_options = re.findall(r"[\d.]+", 
+                           receiver_temp_options_and_unit['values'][0])
+        temp_options = [float(temp) for temp in temp_options]
+        # NOTE: Currently chooses first receiver temp option as default
+        temp = temp_options[0]
+        temp = u.Quantity(temp, receiver_temp_options_and_unit['unit'])
+        return temp
     
