59e5d5084c39bf3c81d5ecb26e9f376b9e76423e
ilgin.okan <ilgin.okan@stfc.ac.uk>
2025-12-15 15:05:39 +0000

    ASC-72 Added Muscat specific methods, added Muscat speciic system temperature calculation methods

--- atlast_sc/instruments/classes/Muscat.py
+++ atlast_sc/instruments/classes/Muscat.py
@@ -1,4 +1,6 @@
+from numpy import expm1, sqrt
 import astropy.units as u
+from astropy import constants
 from atlast_sc.instrument import Instrument
 
 """
@@ -8,6 +10,10 @@ class Muscat(Instrument):
     def __init__(self, data):
         super().__init__(data)
         self._T_rx = self._set_default_receiver_temp(self.receiver_temp_options_and_unit)
+        self.eta_chip_co = self.data.chip_and_cold_optics_efficiency['value']
+        # self.T_co = self.create_Quantity(self.data.cold_optics_temperature) # not currently used
+        self.del_tag = self.create_Quantity(self.data.band_gap)
+        self.eta_pb = self.data.pair_breaking_efficiency['value']
     
     ##################################
     # Instrument specific parameters #
@@ -24,6 +30,64 @@ class Muscat(Instrument):
     ################################################
     # Additional instrument specific methods below #
     ################################################
+
+    @staticmethod
+    def create_Quantity(inst_spec_param):
+        """
+        Create an Astropy Quantity object from a parameter 
+        retrieved from the instrument YAML. 
+
+        Note: Only used on the instrument specific parameters 
+        that have a unit accompanying the value. As some of the 
+        instrument specific equations need to be working with 
+        Astropy Quantity objects.
+        """
+        value = inst_spec_param['value']
+        unit = inst_spec_param['unit']
+        quantity = u.Quantity(value=value, unit=unit)
+        return quantity
+
+    @staticmethod
+    def bose_einstein(obs_freq, temp):
+        """
+        Bose-Einstein photon-occupation number calculation
+        """
+        # expm1(x) = exp(x)-1 
+        return 1/(expm1(constants.h * obs_freq / (constants.k_B * temp)))
+    
+    def calculate_system_temperature(self, obs_freq, bandwidth, T_cmb, eta_eff, T_atm, 
+                                     T_amb, T_sky, transmittance, n_pol):
+        """
+        Returns system temperature, following calculation in [doc]
+
+        :return: system temperature in Kelvin
+        :rtype: astropy.units.Quantity
+        """
+        # calculate power spectral density
+        psdkid = (self.eta_chip_co * (1 - eta_eff) * constants.h *
+                obs_freq * self.bose_einstein(obs_freq, T_amb) +
+                self.eta_chip_co * eta_eff * (1 - transmittance)
+                * constants.h * obs_freq *
+                self.bose_einstein(obs_freq, T_atm) +
+                self.eta_chip_co * eta_eff * transmittance
+                * constants.h * obs_freq *
+                self.bose_einstein(obs_freq, T_cmb)
+                )
+        
+        # calculate power absorbed by instrument
+        pkid = (psdkid * bandwidth) # assuming small bandwidth
+
+        # calculate noise equivalent power
+        nep = (sqrt(2 * pkid * constants.h * obs_freq +
+                    2 * pkid**2 / (n_pol * bandwidth) +
+                    4 * self.del_tag * pkid / self.eta_pb))
+
+        system_temp = (nep / (constants.k_B * eta_eff * transmittance *
+               self.eta_chip_co *
+               sqrt(2 * n_pol * bandwidth))).to(u.K)
+        
+        self.T_sys = system_temp
+        return system_temp
 
     def calculate_receiver_temp(self, obs_freq):
         # NOTE: This will be populated with instrument specific 
